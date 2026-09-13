939e61572e5e16afc3702a98eb8155a2afc373a5
Muskan_Diwan <154693578+Muskavictorn@users.noreply.github.com>
2026-09-13 11:41:39 +0530

    Cloud storage,PDF-Image conversion,Key-Value storage and AI feedback

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: Route.LinksFunction = () => [
 export function Layout({ children }: { children: React.ReactNode }) {
   const { init } = usePuterStore();
   useEffect(() => {
-    init()
+    init();
   }, [init]);
   return (
     <html lang="en">
